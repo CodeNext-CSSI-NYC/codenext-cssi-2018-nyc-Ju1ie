06ebef98b1ed67b456049e0aa9db441116265814
Ju1ie <julie.leonmarcos@csedge.org>
2018-07-12 11:32:11 -0400

    added functions-practice.js

---
+++ functions-practice.js
@@ -0,0 +1,69 @@
+// Julie Leon
+// MAKE SURE YOU CHOOSE GOOD, RELEVANT NAMES FOR YOUR FUNCTIONS AND PARAMETERS
+
+// 1a. Write (declare) a function that prints out "meow"
+
+// function makeSound(){
+//   console.log("meow");
+// }
+// 1b. Call it three times in a row. Run your code to make sure it works.
+
+// makeSound();
+// makeSound();
+// makeSound();
+// 2a. Write (declare) a function that takes one parameter and prints out the square of that number
+
+// function squareRoot(num){
+//   console.log(num ** 2);
+// }
+// 2b. Call it three times with three different parameters. Run your code to make sure it works.
+
+// squareRoot(5);
+// squareRoot(6);
+// squareRoot(7);
+
+// 3a. Write (declare) a function that takes two parameters and prints out
+// "_____ is greater than ____" with the blanks filled in correctly with the two inputs.
+// function greater(num1, num2){
+//    if(num1 > num2){
+//      console.log(num1 + " is greater than " + num2);
+//    } else if (num2 > num1){
+//      console.log(num2 + " is greater than " + num1);
+//    } else {
+//      console.log(num1 + " is equal to " + num2);
+//    }
+//  }
+
+
+// 3b. Call it three times with three different parameters
+// Make sure to try cases where the first one is bigger, the second one is bigger, and when they are equal.
+// Run your code to make sure it works.
+// greater(1 , 1);
+// greater(8 , 6);
+// greater(4 , 6);
+
+// 4a. Write (declare) a function that takes one parameter called name and
+// returns (not prints out, just returns) the string "Hello ____", with the name in the blank.
+
+ // function sayName(name){
+ //   return name;
+ // }
+// 4b. Call the function and store the result in a variable. Then, print out that variable.
+// Repeat this three times with different names.
+
+// let hello = sayName("Julie")
+// console.log("Hello " + hello);
+
+// 5a. Write (declare) a function that takes two parameters and
+// returns (not prints out, just returns) the average of the parameters.
+
+// function average(num1, num2){
+//   return (num1 + num2)/2;
+// }
+// 5b. Call the function and store the result in a variable. Then, print out that variable
+// in a sentence like "The average is _____".
+// Repeat this three times with different numbers. What happens if you pass in
+// something that isn't a number, like true or "apple"?
+
+// let number = average(10, 10);
+// console.log("The average is " + number);
